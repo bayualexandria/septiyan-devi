762dfaaa1b028bc8592f06604a2e0dade267b846
unknown <Bayu>
2024-09-09 15:35:57 +0900

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,7 @@ module.exports = {
         cinzel: ["Cinzel"],
       },
       animation: {
-        "text-slide":
-          "text-slide 1.5s cubic-bezier(0.83, 0, 0.17, 1) infinite",
+        "text-slide": "text-slide 1.5s cubic-bezier(0.83, 0, 0.17, 1) infinite",
         "text-slide-2":
           "text-slide-2 5s cubic-bezier(0.83, 0, 0.17, 1) infinite",
         "text-slide-3":
@@ -31,10 +30,10 @@ module.exports = {
           "text-slide-7 17.5s cubic-bezier(0.83, 0, 0.17, 1) infinite",
         "text-slide-8":
           "text-slide-8 20s cubic-bezier(0.83, 0, 0.17, 1) infinite",
+        "text-slide-1":"text-slide-1 5s"
       },
       keyframes: {
         "text-slide": {
-          
           "0%, 50%": {
             transform: "translateX(0%)",
           },
@@ -42,16 +41,16 @@ module.exports = {
             transform: "translateX(-66.66%)",
           },
         },
-        "text-slide-2": {
-          "0%, 40%": {
-            transform: "translateY(0%)",
-          },
-          "50%, 90%": {
-            transform: "translateY(-33.33%)",
+        "text-slide-1": {
+          "0%": {
+            opacity: "0",
           },
           "100%": {
-            transform: "translateY(-66.66%)",
+            opacity: "1",
           },
+        },
+        "text-slide-2": {
+          animation: "float 6s ease-in-out",
         },
         "text-slide-3": {
           "0%, 26.66%": {
